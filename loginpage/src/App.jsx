df7cb4076927c29b66a5ac6472806bf0f4dbdd3e
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2025-01-03 00:16:09 +0530

    login page UI added

--- loginpage/src/App.jsx
+++ loginpage/src/App.jsx
@@ -4,12 +4,52 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "user" && password === "password") {
+      setError("");
+      setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+    }
+  };
 
   return (
     <>
       
       <h1>login Page</h1>
+      {
+        isSubmitted ? (
+          <div>
+            <p>Welcome, {username}!</p>
+          </div>
+        ) : (
+            <form>
+              <div>
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}
+                placeholder="username"/>
+              </div>
+
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}
+                placeholder="password"/>
+              </div>
+
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+        )
+
+      }
     </>
   )
 }
